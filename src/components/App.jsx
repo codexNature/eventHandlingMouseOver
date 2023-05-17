74df6d1f480023452cf9fd79f93bd7d6f4627d27
naturesola <100572919+naturesola@users.noreply.github.com>
2023-05-17 14:18:15 +0100

    change h1 text when click submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 
 
 function App() {
-
-  const [headingText, setHeadingText] = useState("Hello")
-  const [isMouseOver, setMouseOver] = useState(false)
+  //  eslint-disable-next-line
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
+  const [addname, setAddname] = useState("")
 
   function handleClick() {
-    setHeadingText("Welcome!")
+    setHeadingText("Welcome")
+    setAddname(name);
   }
 
   function handleMouseOver(){
@@ -17,10 +20,19 @@ function App() {
   function handleMouseOut() {
     setMouseOver(false)
   }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>{headingText} {addname}</h1>
+      <input
+      onChange={handleChange}
+      type="text" 
+      placeholder="What's your name?" 
+      value={name}  
+      />
       <button 
       style={{backgroundColor: isMouseOver ? "black" : "white"}}
       onClick={handleClick}
